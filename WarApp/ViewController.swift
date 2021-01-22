5c1f51baa8829993cac1b6e90498459ea6225ee9
Hamas Hassan <hamas.hassan1996@gmail.com>
2021-01-23 03:26:11 +0500

    Initial Commit

---
+++ WarApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarApp
+//
+//  Created by Hamas Hassan on 1/23/21.
+//  Copyright © 2021 Hamas Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
